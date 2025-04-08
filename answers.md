f047a38f2998f80dbf3ab8db4e5678595d0d44dd
cwelch2 <cwelch3@tulane.edu>
2025-04-07 22:57:24 -0500

    update

--- answers.md
+++ answers.md
@@ -1,8 +1,35 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:**_____Cole Welch__________
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
 
+1a)
+A greedy algorithm for producing as few coins as possible that sum to N is to 
+select the highest 2^k of coin that is <=N and log it, then subtract that value from N. If it is not possible
+to select the coin, move on to the next highest denomination of coin and repeat until N is equal to 0, at which point
+you can return the totals for each type of coin.
+
+
+
+1b)
+Greedy Choice Property: the largest possible coin is 
+chosen at each step, this is valid because any other combination of coins
+would result in more coins.
+
+Optimal Substructure: The problem reduces to finding the optimal solution for N - 2^k, so it has optimal substructure.
+
+1c) Both the work and span for this algorithm are O(log n)
+
+
+2a) when coins are [1, 3, 4] and N = 6, the greedy algorithm uses coins 4, 1, 1, which is 3 coins.
+The optimal solution uses 3, 3, which is 2 coins, showing greedy fails.
+
+2b) It still has optimal substructure because the minimum number of coins to make N can be built by
+taking one coin (D_i) and solving the subproblem for N - D_i
+
+2c) a dynamic programming algorithm fills a table dp where dp[i] is the fewest
+coins needed to make i, and for each amount up to N, we try all denominations and set
+dp[i] = min(dp[i], 1 + dp[i-D_i]). The work is O(N k) and the span is O(N)
--- main.py
+++ main.py
@@ -7,23 +7,65 @@ test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relev
 alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
+    if S == "":
+        return len(T)
+    elif T == "":
+        return len(S)
+    elif S[0] == T[0]:
+        return MED(S[1:], T[1:])
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+        insert = MED(S, T[1:])
+        delete = MED(S[1:], T)
+        substitute = MED(S[1:], T[1:])
+        return 1 + min(insert, delete, substitute)
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+    # Top-down memoization
+    key = (S, T)
+    if key in MED:
+        return MED[key]
+    if S == "":
+        MED[key] = len(T)
+    elif T == "":
+        MED[key] = len(S)
+    elif S[0] == T[0]:
+        MED[key] = fast_MED(S[1:], T[1:], MED)
+    else:
+        insert = fast_MED(S, T[1:], MED)
+        delete = fast_MED(S[1:], T, MED)
+        substitute = fast_MED(S[1:], T[1:], MED)
+        MED[key] = 1 + min(insert, delete, substitute)
+    return MED[key]
+
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    # Top-down memoization with alignment tracking
+    key = (S, T)
+    if key in MED:
+        return MED[key]
+
+    if S == "":
+        MED[key] = (len(T), ("-" * len(T), T))
+        return MED[key]
+    if T == "":
+        MED[key] = (len(S), (S, "-" * len(S)))
+        return MED[key]
+    if S[0] == T[0]:
+        dist, (aS, aT) = fast_align_MED(S[1:], T[1:], MED)
+        MED[key] = (dist, (S[0] + aS, T[0] + aT))
+    else:
+        # Try all three options: insert, delete, substitute
+        insert_dist, (insert_S, insert_T) = fast_align_MED(S, T[1:], MED)
+        delete_dist, (delete_S, delete_T) = fast_align_MED(S[1:], T, MED)
+        sub_dist, (sub_S, sub_T) = fast_align_MED(S[1:], T[1:], MED)
+
+        # Add the edit and choose the best
+        insert = (1 + insert_dist, ("-" + insert_S, T[0] + insert_T))
+        delete = (1 + delete_dist, (S[0] + delete_S, "-" + delete_T))
+        substitute = (1 + sub_dist, (S[0] + sub_S, T[0] + sub_T))
+
+        MED[key] = min(insert, delete, substitute, key=lambda x: x[0])
+
+    return MED[key]
 
